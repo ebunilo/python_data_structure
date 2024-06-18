2d374bfd0abb9a5776b94338bcccb45b2269c49b
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2024-06-18 10:01:02 +0100

    add docstring

--- sorted_squared.py
+++ sorted_squared.py
@@ -1,5 +1,17 @@
 def sorted_squared(array):
-    #write code here.make sure to return desired array
+    """
+    Sorts the squared values of the input array in ascending order.
+
+    Args:
+        array (list): A list of integers.
+
+    Returns:
+        list: A list of squared integers in ascending order.
+
+    Example:
+        >>> sorted_squared([-3, 2, 4, 6])
+        [4, 9, 16, 36]
+    """
     
     array_squared = [x**2 for x in array]
     n = len(array_squared)
